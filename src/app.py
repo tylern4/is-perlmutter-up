6bc53aefc298c7a78c5f282e99130bb9f70f1cb4
Nick Tyler <nicholas.s.tyler.4@gmail.com>
2022-11-17 12:33:21 -0600

    Merge to less files

--- src/app.py
+++ src/app.py
@@ -1,7 +1,103 @@
-"""App entry point."""
-from . import create_app
+import emoji as emj
+import random
+import json
+import requests
+from flask import render_template
+from flask import current_app as app
+from flask import Flask
+
+
+def create_app():
+    """Construct the core application."""
+    app = Flask(__name__, template_folder="templates")
+
+    with app.app_context():
+        return app
+
 
 app = create_app()
+
+"""Route declaration."""
+
+
+def get_perlmutter_status_title(name: str = "perlmutter"):
+    print(f"###### {name}  ######", flush=True)
+    respose = requests.get(f'https://api.nersc.gov/api/v1.2/status/{name}',
+                           headers={'accept': 'application/json'})
+
+    # app.logger.info(respose.text)
+    print(f"###### respose.text = {respose.text}", flush=True)
+    data = json.loads(respose.text)
+
+    return {
+        'active': data['status'],
+        'notes': data['notes']
+    }
+
+
+good_emoji = [':thumbs_up:',
+              ':thumbs_up_dark_skin_tone:',
+              ':thumbs_up_light_skin_tone:',
+              ':thumbs_up_medium-dark_skin_tone:',
+              ':thumbs_up_medium-light_skin_tone:',
+              ':thumbs_up_medium_skin_tone:',
+              ':rocket:',
+              ':T-Rex:',
+              ':bridge_at_night:',
+              ':check_mark_button:',
+              ]
+bad_emoji = [':thumbs_down:',
+             ':SOS_button:',
+             ':ambulance:',
+             ':angry_face:',
+             ':angry_face_with_horns:',
+             ':cold_face:',
+             ':enraged_face:',
+             ':collision:',
+             ':confounded_face:',
+             ':crying_cat:',
+             ':crying_face:',
+             ':disappointed_face:',
+             ':down_arrow:',
+             ':face_vomiting:'
+             ':face_vomiting:',
+             ':face_with_crossed-out_eyes:',
+             ':face_with_diagonal_mouth:',
+             ':face_with_head-bandage:',
+             ':face_with_open_mouth:',
+             ':face_with_peeking_eye:',
+             ':face_with_raised_eyebrow:',
+             ':face_with_rolling_eyes:',
+             ':face_with_spiral_eyes:',
+             ':face_with_steam_from_nose:',
+             ':face_with_symbols_on_mouth:',
+             ':face_with_thermometer:',
+             ]
+
+
+@app.route('/', defaults={'name': 'perlmutter'})
+@app.route('/<path:name>')
+def home(name):
+    status = get_perlmutter_status_title(name)
+    if status['active']:
+        color = "MediumSeaGreen"
+        emoji = random.choice(good_emoji)
+        notes = ""
+        title = f"YES! {name} is up!"
+    else:
+        color = "#A93226"
+        emoji = random.choice(bad_emoji)
+        notes = status['notes']
+        title = f"NO! {name} is down!"
+
+    return render_template(
+        "home.html",
+        description=emj.emojize(emoji),
+        notes=notes,
+        color=color,
+        title=title,
+    )
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
