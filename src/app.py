9e7901ec7a5521cb91ade9d22a709b0d2f422c22
Nick Tyler <tylern@canisius.edu>
2022-12-12 15:01:28 -0500

    Add states

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+import time
 import emoji as emj
 import random
 import json
@@ -5,38 +6,7 @@ import requests
 from flask import render_template
 from flask import current_app as app
 from flask import Flask
-
-
-def create_app():
-    """Construct the core application."""
-    app = Flask(__name__, template_folder="templates")
-
-    with app.app_context():
-        return app
-
-
-app = create_app()
-
-"""Route declaration."""
-
-
-def get_perlmutter_status_title(name: str = "perlmutter"):
-    app.logger.info(name)
-    if name == 'favicon.ico':
-        return None
-
-    respose = requests.get(f'https://api.nersc.gov/api/v1.2/status/{name}',
-                           headers={'accept': 'application/json'})
-
-    app.logger.info(respose.text)
-    data = json.loads(respose.text)
-    app.logger.debug(data)
-
-    return {
-        'active': (data['status'] == 'active'),
-        'description': data['description'],
-        'notes': data['notes']
-    }
+from enum import Enum
 
 
 good_emoji = [':thumbs_up:',
@@ -50,24 +20,25 @@ good_emoji = [':thumbs_up:',
               ':bridge_at_night:',
               ':check_mark_button:',
               ]
+maybe_emoji = [':confounded_face:',
+               ':ambulance:',
+               ':disappointed_face:',
+               ':face_with_head-bandage:',
+               ':face_with_thermometer:',
+               ]
 bad_emoji = [':thumbs_down:',
              ':SOS_button:',
-             ':ambulance:',
              ':angry_face:',
              ':angry_face_with_horns:',
              ':cold_face:',
              ':enraged_face:',
              ':collision:',
-             ':confounded_face:',
              ':crying_cat:',
              ':crying_face:',
-             ':disappointed_face:',
              ':down_arrow:',
-             ':face_vomiting:'
              ':face_vomiting:',
              ':face_with_crossed-out_eyes:',
              ':face_with_diagonal_mouth:',
-             ':face_with_head-bandage:',
              ':face_with_open_mouth:',
              ':face_with_peeking_eye:',
              ':face_with_raised_eyebrow:',
@@ -75,8 +46,77 @@ bad_emoji = [':thumbs_down:',
              ':face_with_spiral_eyes:',
              ':face_with_steam_from_nose:',
              ':face_with_symbols_on_mouth:',
-             ':face_with_thermometer:',
              ]
+
+
+class SystemState(Enum):
+    ACTIVE = 1
+    DOWN = 2
+    DEGRADED = 3
+    MAINTNAINCE = 4
+    UNKNOWN = 5
+
+
+def create_app():
+    """Construct the core application."""
+    app = Flask(__name__, template_folder="templates")
+
+    with app.app_context():
+        return app
+
+
+app = create_app()
+
+
+def get_perlmutter_status_title(name: str = "perlmutter"):
+    app.logger.info(name)
+    if name == 'favicon.ico':
+        return {'state': SystemState.UNKNOWN,
+                'notes': f'Status of {name} unknown'
+                }
+
+    respose = requests.get(f'https://api.nersc.gov/api/v1.2/status/{name}',
+                           headers={'accept': 'application/json'})
+
+    app.logger.info(respose.text)
+    try:
+        data = json.loads(respose.text)
+    except json.decoder.JSONDecodeError:
+        return {'state': SystemState.UNKNOWN,
+                'notes': f'Status of {name} unknown'
+                }
+    app.logger.debug(data)
+
+    if data['status'] == 'active':
+        state = SystemState.ACTIVE
+        if data['description'] == "System Degraded":
+            state = SystemState.DEGRADED
+    else:
+        state = SystemState.DOWN
+        if data['description'] == "Scheduled Maintenance":
+            state = SystemState.MAINTNAINCE
+
+    event_time, notes = get_notes(data['notes'])
+    return {
+        'state': state,
+        'active': (data['status'] == 'active'),
+        'description': data['description'],
+        'notes': notes,
+        'time': event_time
+    }
+
+
+def get_notes(notes):
+    try:
+        notes = notes[0]
+        tmp = notes.split(",")
+        event_time = tmp[0]
+        notes = tmp[-1]
+    except Exception as err:
+        app.logger.error(f"{type(err).__name__}: {err}")
+        return time.time(), ""
+
+    return event_time, notes
 
 
 @app.route('/', defaults={'name': 'perlmutter'})
@@ -85,22 +125,33 @@ def home(name):
     if name == 'favicon.ico':
         return render_template("home.html")
     status = get_perlmutter_status_title(name)
-    if 'active' in status and status['active']:
-        if status['description'] == "System Degraded":
-            color = "Coral"
-            emoji = random.choice(good_emoji)
-            notes = status['notes']
-            title = f"Maybe? {name} is degraded."
-        else:
+
+    match status['state']:
+        case SystemState.ACTIVE:
             color = "MediumSeaGreen"
             emoji = random.choice(good_emoji)
             notes = ""
             title = f"YES! {name} is up!"
-    else:
-        color = "#A93226"
-        emoji = random.choice(bad_emoji)
-        notes = status['notes']
-        title = f"NO! {name} is down!"
+        case SystemState.DEGRADED:
+            color = "Coral"
+            emoji = random.choice(maybe_emoji)
+            notes = status['notes']
+            title = f"Maybe? {name} is degraded."
+        case SystemState.DOWN:
+            color = "#A93226"
+            emoji = random.choice(bad_emoji)
+            notes = status['notes']
+            title = f"NO! {name} is down!"
+        case SystemState.MAINTNAINCE:
+            color = "RoyalBlue"
+            emoji = random.choice(maybe_emoji)
+            notes = status['notes']
+            title = f"No, {name} is down for maintenance."
+        case SystemState.UNKNOWN:
+            color = "MediumPurple"
+            emoji = random.choice(bad_emoji)
+            notes = status['notes']
+            title = f"System {name} not found!"
 
     return render_template(
         "home.html",
